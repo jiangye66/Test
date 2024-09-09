99f3667ff33038a2a067da9e1d3787828bad995d
Jiangye <2831185361@qq.com>
2024-09-09 14:46:10 +0800

    第二次修改

--- Test/src/Main.java
+++ Test/src/Main.java
@@ -16,7 +16,7 @@ public class Main {
         }
     }
     public static void main(String[] args) {
-        int[] nums = new int[]{1,2,3,4,5,6,7,8,9};
+        int[] nums = new int[]{1,2,3,4,5,6};
         TreeNode root = createTree(nums,0);
         dfs(root);
     }
